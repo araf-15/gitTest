44dc08fe52b24e37e9a600849184154613f1f76d
araf-15 <hasan.araf15@gmail.com>
2021-02-04 19:01:39 +0600

    Integrate ref keyword example

--- TestApplication/TestApplication/Program.cs
+++ TestApplication/TestApplication/Program.cs
@@ -6,7 +6,17 @@ namespace TestApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 10;
+            int b = 20;
+            MyMath(ref a, ref b);
+            Console.WriteLine("Value of a: "+a);
+            Console.WriteLine("Value of b: "+b);
+        }
+
+        static void MyMath(ref int a, ref int b)
+        {
+            a = a + b;
+            b = a - b;
         }
     }
 }
